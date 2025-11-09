242ab576c44bd115ce1ca9ef41b20df4eccd9ec4
alexhoangkhai <alextran170705@gmail.com>
2025-11-08 20:27:18 -0500

    alex commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,66 @@
-import { useState, useEffect, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styling/App.css'
-import LeftComponent from './components/LeftComponent.jsx';
-import RightComponent from './components/RightComponet.jsx';
-import Demo from './components/Demo.jsx'
-import Switcher4 from './components/Switcher4.jsx'
+  import { useState, useEffect, useRef } from 'react'
+  import reactLogo from './assets/react.svg'
+  import viteLogo from '/vite.svg'
+  import './styling/App.css'
+  import LeftComponent from './components/LeftComponent.jsx';
+  import RightComponent from './components/RightComponet.jsx';
+  import Demo from './components/Demo.jsx'
+  import Switcher4 from './components/Switcher4.jsx'
+  import OptionDropdown from './components/OptionDropdown.jsx'
 
 
-function App() {
+  function App() {
 
-  const [camera_state, set_camera_state] = useState(false)
-  
-  const demo = false
+    const [camera_state, set_camera_state] = useState(false)
+    const [showDemo, setShowDemo] = useState(false)    
+    const [selected, setSelected] = useState("Translation Option")
 
 
-  function toggleCamera() {
-    if (camera_state)
-      set_camera_state(false)
-    else
-      set_camera_state(true)
+    function toggleCamera() {
+      if (camera_state)
+        set_camera_state(false)
+      else
+        set_camera_state(true)
+    }
+
+    function handleToggleDemo() {
+    setShowDemo(prev => !prev)  // toggle Demo component
   }
 
+    return (
+      <>
+        <div className="navbar border-b-2 w-full h-20 flex flex-row items-center justify-between px-4">
+          <div className='project-name text-5xl'>
+            SynSight
+          </div>
 
-  function showGlasses(){
-    
+          <button className='turn-on-camera-btn' onClick={toggleCamera}>
+            turn {camera_state ? "off" : "on"} camera
+          </button>
+        </div>
+
+        <div className="main-container">
+
+          <div className="flex gap-6">
+            <div className="left_box w-1/2 h-100 rounded-lg border border-3">
+              <LeftComponent camera_state={camera_state} />
+            </div>
+            <div className="right_box w-1/2 h-100 rounded-lg border border-3">
+              <RightComponent />
+            </div>
+          </div>
+
+          <OptionDropdown selected={selected} setSelected={setSelected}/>
+
+          <Switcher4 isChecked={showDemo} onChange={handleToggleDemo} />
+
+
+          {showDemo && <Demo />}
+
+        </div>
+      </>
+
+    )
   }
 
-  return (
-    <>
-      <div className="navbar border-b-2 w-full h-20 flex flex-row items-center justify-between px-4">
-        <div className='project-name text-5xl'>
-          SynSight
-        </div>
-
-        <button className='turn-on-camera-btn' onClick={toggleCamera}>
-          turn {camera_state ? "off" : "on"} camera
-        </button>
-      </div>
-
-      <div className="main-container">
-
-<<<<<<< HEAD
-        <h1 className='text-3xl font-bold text-white-300 text-center mb-4'>Welcome to SynSight</h1>
-
-        <div className="flex gap-6">
-          <div className="w-1/2 h-100 rounded-lg border border-3">
-=======
-        <div className="flex">
-          <div className="left_box w-1/2 h-100 rounded-lg border border-3">
->>>>>>> d1e8e44e473d24356e517770fcee02f40a57f217
-            <LeftComponent camera_state={camera_state} />
-          </div>
-          <div className="right_box w-1/2 h-100 rounded-lg border border-3">
-            <RightComponent />
-          </div>
-        </div>
-        
-        <Switcher4/>
-
-        <Demo/>
-
-      </div>
-    </>
-
-  )
-}
-
-export default App
+  export default App
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,20 @@
 
+import bgImage from '../assets/Gemini_Generated_Image_ef8v4ref8v4ref8v.png';
+import fgImage from '../assets/Background-Removed.png';
 
 export default function Demo(props){
 
     return (
-    <div className="demo-container w-full h-96 border mx-auto block mt-6 bg-cover bg-center"
-      style={{ backgroundImage: `url('../assets/')` }}>
-       
+    <div className="demo-container w-full h-96 border mx-auto block mt-6 bg-cover bg-center rounded-lg border border-3 relative overflow-hidden"
+      style={{ backgroundImage: `url(${bgImage})` }}>
+
+       <img
+        src={fgImage}
+        alt="foreground"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-56"
+        style={{ transform: ' scale(1.45)' }}
+      />
+
     </div>
     )
 }
